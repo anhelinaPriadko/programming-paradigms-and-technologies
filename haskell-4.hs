1f6138c1ebf1d758dd1da75b5da6632661ddb3a9
Linaa <anhelina.pryadko@gmail.com>
2026-04-29 21:00:18 +0000

    add tests for haskell-4 and prolog-3

--- haskell-4.hs
+++ haskell-4.hs
@@ -17,29 +17,29 @@ checkNonTerminal (_, rhsList) =
 isKorenyakHopcroft :: [Rule] -> Bool
 isKorenyakHopcroft grammar = all checkNonTerminal grammar
 
+runTestCase :: String -> [Rule] -> IO ()
+runTestCase name grammar = do
+    putStrLn $ "=== " ++ name ++ " ==="
+    putStrLn $ "Вхідні правила: " ++ show grammar
+    if isKorenyakHopcroft grammar
+        then putStrLn "РЕЗУЛЬТАТ: ТАК (Граматика відповідає умовам)\n"
+        else putStrLn "РЕЗУЛЬТАТ: НІ (Умови не виконано: дублі або нетермінал на початку)\n"
+
 main :: IO ()
 main = do
     hSetEncoding stdout utf8
     hSetEncoding stdin utf8
-
-    putStrLn "--- Перевірка граматики Кореняка-Хопкрофта ---"
-    putStrLn "Введіть кількість нетерміналів:"
-    hFlush stdout
-    nStr <- getLine
-    let n = read nStr :: Int
-
-    grammar <- readGrammar n
     
-    if isKorenyakHopcroft grammar
-        then putStrLn "РЕЗУЛЬТАТ: Так, це граматика Кореняка-Хопкрофта."
-        else putStrLn "РЕЗУЛЬТАТ: Ні, умови не виконано (повтор перших символів або правило не з терміналу)."
+    putStrLn "---ТЕСТУВАННЯ ГРАМАТИК КОРЕНЯКА-ХОПКРОФТА ---\n"
 
-readGrammar :: Int -> IO [Rule]
-readGrammar 0 = return []
-readGrammar n = do
-    putStrLn "Введіть нетермінал (велика літера) та правила через пробіл (наприклад: A aB aC):"
-    hFlush stdout
-    line <- getLine
-    let (nt:rules) = words line
-    rest <- readGrammar (n - 1)
-    return ((head nt, rules) : rest)
+    runTestCase "Тест 1:" 
+                [('S', ["aA", "b"]), ('A', ["cS", "d"])]
+
+    runTestCase "Тест 2:" 
+                [('S', ["xA", "xB"])]
+
+    runTestCase "Тест 3:" 
+                [('S', ["Ax"]), ('A', ["a"])]
+
+    runTestCase "Тест 4:" 
+                [('S', ["xA", "y"]), ('A', ["zS", "zB"])]
